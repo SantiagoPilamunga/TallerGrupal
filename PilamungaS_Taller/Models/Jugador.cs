a1f868728bd10c81546e5a6064f542e3170224d6
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-04-30 15:14:56 -0500

    Agregamos los modelos de jugador y reporte

---
+++ PilamungaS_Taller/Models/Jugador.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PilamungaS_Taller.Models
+{
+    public class Jugador
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumeroCamiseta { get; set; }
+        public int Goles {  get; set; }
+        public int AsistenciasTotales { get; set; }
+        public float Sueldo { get; set; }
+    }
+}
---
+++ PilamungaS_Taller/Models/Reporte.cs
@@ -0,0 +1,15 @@
+﻿namespace PilamungaS_Taller.Models
+{
+    public class Reporte
+    {
+        public List<Jugador> Goleadores {  get; set; }
+        public List<Jugador> Asistentes { get; set; }
+        public List<Jugador> EquipoMayorPresupuesto { get; set; }
+
+    }
+    public class EquipoMayorPresupuesto
+    {
+        public String NombreEquipo { get; set; }
+        public float Presupuesto { get; set; }
+    }
+}
